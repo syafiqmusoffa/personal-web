dd1813f82011decda71a6c9afe1cf47d04ff1dc3
muhammad musoffa <syafiqmusoffa@gmail.com>
2025-02-13 01:38:12 +0700

    feat: express & handlebars integrated

--- assets/js/hello-world.js
+++ assets/js/hello-world.js
@@ -12,3 +12,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server berjalan di port ${port}`);
   });
+  
--- server.js
+++ server.js
@@ -1,26 +1,65 @@
 const express = require("express");
 const app = express();
-const port = 3002;
+const port = 3000;
 
 app.set("view engine", "hbs");
 
 app.use(express.static("assets"));
 
 // HOME
+
 app.get("/", (req, res)=> {
-    res.render("index");
+    res.render("blog")
+ })
+
+app.get("/index", (req, res)=> {
+    // const id = req.params.id
+    // res.send(`Halo! ini halaman utama`);
+   res.render("index")
 })
 
-// CONTACT ME
-app.get("/contact", (req, res) => {
-    res.render("contact");
-})
+// Blog
 
-// BLOG
-app.get("/blog", (req, res) => {
-    res.render("blog");
-})
+app.get("/blog", (req, res)=> {
+    res.render("blog")
+ })
 
+// Blog-detail
+
+app.get("/blog-detail", (req, res)=> {
+    res.render("blog-detail")
+ })
+ 
+// Testimonials
+
+app.get("/testimonials", (req, res)=> {
+    res.render("testimonials")
+ })
+ 
+// Contact
+
+app.get("/contact", (req, res)=> {
+    res.render("contact")
+ }) 
+
+// REQUEST PARAMS
+// app.get("/about/:id", (req, res) => {
+//     res.send(`Halo! ini halaman tentang ${id}`);
+// })
+
+// REQUEST QUERY
+// app.get("/blog?title=javascript", (req, res) => {
+//     // const title = req.query.title;
+//     // const author = req.query.author;
+//     // const year = req.query.year;
+
+//     const {title, author, year} = req.query
+//     // console.log(req.query)
+//     // res.send("ini halaman blog" + title + "dengan author" + author "tahun" + year);
+//     res.send(`ini halaman blog ${title} dengan author ${author} tahun ${year}`);
+
+//     // res.send("ok");
+// })
 
 
 
